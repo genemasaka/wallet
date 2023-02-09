dbe2d8783a81744c2ef563504ee8e4f5274fb036
Eugene Masaka <eugenemasaka777@gmail.com>
2023-02-09 10:49:59 +0300

    UI and Logic

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,61 @@
 use yew::prelude::*;
 use yew::{classes};
 
+#[derive(Debug)]
 struct User {
 	name: String,
-	uid: u64;
-	balance: u32;
+	uid: u64,
+	balance: u32,
 }
 
-impl User {
-	fn send(&self) {
-		
-	}
-}
 
 #[function_component]
 fn App() -> Html {
 	let sender = User {
-		String::from("Gene Masaka"),
-		1331997,
-		500,
-	}
+		name: "Gene Masaka".to_string(),
+		uid: 1331997,
+		balance: 500,
+	};
 	
 	let recepient = User {
-		String::from("Vitalik"),
-		1998,
-		0,
+		name: String::from("Vitalik"),
+		uid: 1998,
+		balance: 0,
+	};
+
+	let sender_balance = use_state(|| sender.balance);
+	let onclick = {
+		let amount: u32 = 33;
+		let balance = sender_balance.clone();
+
+		match sender.uid {
+		1331997 => {
+			let value = *balance - amount;
+      		sender_balance.set(value);
+			},
+			_=> println!("You dont have the correct credentials to transfer tokens"),
+		}
+		
+	
+	};
+
+
+	html! {
+		<>
+		<div class={classes!("main")}>
+		<div class={classes!("card")}>
+		<div class={classes!("card-header")}>
+			<h3>{"Wallet"}</h3>
+		</div>
+		<div class={classes!("card-body")}>
+		<h5>{"New Balance:"}</h5>
+		<p>{ *sender_balance }</p>
+		<button class={classes!("btn")} {onclick}>{"Send"}</button>
+
+		</div>
+		</div>
+		</div>
+		</>
 	}
 
 }
